08958e1f7f4d04c217f8b96ba455298e3afe3fff
Amit kumar <amitbhrampuriya@gmail.com>
2024-06-16 09:59:19 +0530

    initial commit

---
+++ src/config/endpoints.js
@@ -0,0 +1,14 @@
+export const endpoints = {
+    account: {
+      login: "api/user/login",
+      signup: "api/user/signup",
+      user: "api/user/auth",
+    },
+    tour: {
+      bookTour: "api/tour/booktour", // post request provide token also
+      approvedTour: "api/tour/approvedTour", // post request provide admin token also
+      cancelTour: "api/tour/cancelTour", // post request provide token also
+      addNewTour: "api/tour/addTour", // post request provide admin token also
+    },
+  };
+  
